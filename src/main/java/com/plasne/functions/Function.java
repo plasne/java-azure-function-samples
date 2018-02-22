4622003859c93f98d62b40c487c9a281edb660b1
Peter Lasne <plasne@iname.com>
2018-02-22 11:41:18 -0500

    Update Function.java
    fixed spacing

--- src/main/java/com/plasne/functions/Function.java
+++ src/main/java/com/plasne/functions/Function.java
@@ -9,20 +9,20 @@ public class Function {
     @FunctionName("produce")
     public static String produce(
         @TimerTrigger(name = "schedule", schedule = "0 */1 * * * *") String timerInfo,
-	    @BlobInput(name = "blob", connection = "AzureWebJobsStorage", dataType = "binary", path = "input/config.json") byte[] myblob,
+	@BlobInput(name = "blob", connection = "AzureWebJobsStorage", dataType = "binary", path = "input/config.json") byte[] myblob,
         @QueueOutput(name = "message", queueName = "myqueue", connection = "AzureWebJobsStorage") OutputBinding<String> queue,
         final ExecutionContext context) {
 
-        // read message
-    	String value = new String(myblob);
+	// read message
+	String value = new String(myblob);
 
-	    // queue a message
-	    String message = value;
-	    queue.setValue(message);
+	// queue a message
+	String message = value;
+	queue.setValue(message);
 
-     	// return
-	    context.getLogger().info("queued: " + message); 
-        return "queued: " + message;
+	// return
+	context.getLogger().info("queued: " + message); 
+	return "queued: " + message;
     }
 
     @FunctionName("consume")
@@ -30,9 +30,9 @@ public class Function {
         @QueueTrigger(name = "message", queueName = "myqueue", connection = "AzureWebJobsStorage") String message,
         final ExecutionContext context) {
 
-	    // return message
-	    context.getLogger().info("consumed: " + message);
-	    return "consumed: " + message;
+	// return message
+	context.getLogger().info("consumed: " + message);
+	return "consumed: " + message;
 
     }
 
